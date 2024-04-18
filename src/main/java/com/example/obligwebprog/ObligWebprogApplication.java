998ae8db5f1686110dc79a465a077822de794413
KilledByACake <elisabeth0705@live.no>
2024-04-18 12:18:34 +0200

    oblig 3?

---
+++ src/main/java/com/example/obligwebprog/ObligWebprogApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligwebprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligWebprogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligWebprogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obligwebprog/ObligWebprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligwebprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligWebprogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
